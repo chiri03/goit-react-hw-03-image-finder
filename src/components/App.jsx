d44878c29c06d0c1d3a791d689084361b9fc3577
chiri03 <lena.kiriac.97@gmail.com>
2023-08-24 00:04:08 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
 import SearchBar from '../components/Searchbar/SearchBar';
 import ImageGallery from '../components/ImageGallery/ImageGallery';
+import Button from '../components/Button/Button';
+import Loader from '../components/Loader/Loader';
+import Modal from '../components/Modal/Modal';
+
+
+
 
 
 const API_KEY = '37237268-001c8e95be6ecdc276e5b14c7';
@@ -10,8 +16,9 @@ export const App = () => {
   <div>
     <SearchBar />
     <ImageGallery/>
-        
-    
+      <Button></Button> 
+      <Loader></Loader>  
+      <Modal></Modal>
   </div>
   );
 };
